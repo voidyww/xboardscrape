a989e32a4ff454d54118000762a293609bd5fd04
voidyww <yehiawael2005@gmail.com>
2025-04-06 06:36:10 +0200

    fix

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.post("/scrape", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Scraper running on port ${PORT}`);
 });
